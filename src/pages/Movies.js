bdebf2b2c84fa05efbd9b2f1d36041ddc122ab07
che-ri <rnjs1135@gmail.com>
2021-05-03 00:01:00 +0900

    [fix] Popular 장르 추가

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -8,31 +8,33 @@ import { Link } from "react-router-dom";
 const Movies = () => {
   const [isLoading, setIsloading] = useState(true);
   const [data, setData] = useState([]);
-  const [popular, setPopular] = useState({});
+  const [popular, setPopular] = useState([]);
   const GetMovie = async () => {
     //themoviedb에서 인기있는 영화목록을 가져왔다.
     const {
-      data: { results }
+      data: { results },
     } = await axios.get(
       "https://api.themoviedb.org/3/movie/popular?api_key=d7d1e7b43a6581ecf72c793c35505402"
     );
 
     const {
-      data: { genres }
+      data: { genres },
     } = await axios.get(
       "https://api.themoviedb.org/3/genre/movie/list?api_key=d7d1e7b43a6581ecf72c793c35505402&language=en-US"
     );
 
-    const newMovieData = results.map(({ genre_ids: genreIds, ...movieItem }) => {
-      const matchGenres = genres.filter((genre) => genreIds.includes(genre.id));
-      const newMovie = { ...movieItem, genres: matchGenres };
-      return newMovie;
-    });
-
-    // console.log(newPopulars);
+    const newMovieData = results.map(
+      ({ genre_ids: genreIds, ...movieItem }) => {
+        const matchGenres = genres.filter((genre) =>
+          genreIds.includes(genre.id)
+        );
+        const newMovie = { ...movieItem, genres: matchGenres };
+        return newMovie;
+      }
+    );
 
     //state에 영화목록을 저장한다.
-    setPopular(newMovieData.shift());
+    setPopular([newMovieData.shift()]);
     setData(newMovieData);
     setIsloading(false);
     //영화 포스터의 값을 얻고자 했다.
@@ -43,9 +45,9 @@ const Movies = () => {
   //마운트가 끝나자마자 API를 가져올 것이다.
   useEffect(() => {
     GetMovie();
-    // data.map((item) => console.log(item.poster_path));
+    console.log(data);
+    console.log(popular);
   }, []);
-
 
   const StyledPopularMovie = Styled.main`
         position:relative;
@@ -85,6 +87,7 @@ const Movies = () => {
                 display:flex;
                 justify-content:space-between;
                 a{
+                    margin-top:auto;
                     color:#fff;
                     border-bottom: 1px solid #fff
                 }
@@ -96,18 +99,19 @@ const Movies = () => {
   return (
     <Layout>
       <StyledPopularMovie>
-              {popular.map((popularMovie) => (
-                      <PopularData
-                    //data안에 사용할 정보들을 prop으로 선언해주었다.
-                    key={popularMovie.id}
-                    id={popularMovie.id}
-                    title={popularMovie.title}
-                    overview={popularMovie.overview}
-                    vote_average={popularMovie.vote_average}
-                    vote_count={popularMovie.vote_count}
-                    poster_path={`https://image.tmdb.org/t/p/w300/${popularMovie.poster_path}`}
-                    genres={popularMovie.genres.map(genre=>genre.name)}
-                  />))}
+        {popular?.map((popularMovie) => (
+          <PopularData
+            //data안에 사용할 정보들을 prop으로 선언해주었다.
+            key={popularMovie.id}
+            id={popularMovie.id}
+            title={popularMovie.title}
+            overview={popularMovie.overview}
+            vote_average={popularMovie.vote_average}
+            vote_count={popularMovie.vote_count}
+            poster_path={`https://image.tmdb.org/t/p/w300/${popularMovie.poster_path}`}
+            genres={popularMovie.genres.map((genre) => genre.name)}
+          />
+        ))}
       </StyledPopularMovie>
       <section className="movies__container">
         {isLoading ? (
@@ -126,7 +130,7 @@ const Movies = () => {
                 vote_average={movie.vote_average}
                 vote_count={movie.vote_count}
                 poster_path={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
-                genres={movie.genres.map(genre=>genre.name)}
+                genres={movie.genres.map((genre) => genre.name)}
               />
             ))}
           </div>
@@ -143,34 +147,34 @@ function PopularData({
   vote_average,
   vote_count,
   poster_path,
-  genres
-}){
-    return(
-<>
-    <div className="popular__img">
-              <img
-                src={`https://image.tmdb.org/t/p/original/`}
-                alt={title}
-                title={title}
-              />
-            </div>
-            <div className="popular__summary">
-              <h3>Popular Movie</h3>
-              <div className="popular__info">
-                <h4>{title}</h4>
-                <p>{overview}</p>
-              </div>
-              <div className="popular__controls">
-                <span>
-                  평점 ★{vote_average}
-                  <span> ({vote_count}명)</span>
-                  <p><span></span></p>
-                   </span>
-                <Link to="/test">더보기</Link>
-              </div>
-            </div>
-</>
-    )
+  genres,
+}) {
+  return (
+    <>
+      <div className="popular__img">
+        <img
+          src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+          alt={title}
+          title={title}
+        />
+      </div>
+      <div className="popular__summary">
+        <h3>Popular Movie</h3>
+        <div className="popular__info">
+          <h4>{title}</h4>
+          <p>{overview}</p>
+        </div>
+        <div className="popular__controls">
+          <span>
+            평점 ★{vote_average}
+            <span> ({vote_count}명)</span>
+            <p>장르 : {genres.join(" ")}</p>
+          </span>
+          <Link to="/test">더보기</Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
 function MoviesData({
@@ -180,7 +184,7 @@ function MoviesData({
   vote_average,
   vote_count,
   poster_path,
-  genres
+  genres,
 }) {
   //data를 잘 가져왔다면, data 안에 title을 렌더링 할 것이다.
   return (
@@ -189,7 +193,7 @@ function MoviesData({
       <p>
         평점 ★{vote_average}
         <span> ({vote_count}명)</span>
-        <span>장르:{genres.join(' ')}</span>
+        <span>장르 : {genres.join(" ")}</span>
       </p>
       <h3 className="movie__title">{title}</h3>
       <p className="movie__summary">{overview}</p>
@@ -198,12 +202,21 @@ function MoviesData({
 }
 
 //data가 타입에 맞게 잘 가져왔는지 확인하는 작업이다.
+PopularData.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  overview: PropTypes.string.isRequired,
+  vote_average: PropTypes.number.isRequired,
+  vote_count: PropTypes.number.isRequired,
+};
+
+//data가 타입에 맞게 잘 가져왔는지 확인하는 작업이다.
 MoviesData.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   overview: PropTypes.string.isRequired,
   vote_average: PropTypes.number.isRequired,
-  vote_count: PropTypes.number.isRequired
+  vote_count: PropTypes.number.isRequired,
 };
 
 export default Movies;
